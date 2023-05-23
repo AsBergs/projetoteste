b76fcdb4e12869d61a0f39bafd819df6b60eb5eb
JuliaOliveira26 <julinhalves2006@gmail.com>
2023-05-23 11:58:18 -0300

    WHOSHOULDRUDELYBARGEYOUIN

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import {produtos} from '@/_data/produtos'
+</script>
+
+<template>
+<h1>Taylor Swift Store</h1>
+<div class="listagem-produtos">
+<div v-for=" produto in produtos" :key="produto.id" class="card-produto">
+  <img :src="produto.image" alt="imagem" class="produto imagem">
+  <p class="produto-name">{{ produto.nome }}</p>
+  <p class="produto-preco">{{ produto.price }}</p>
+
+</div>
+
+</div>
+
+
+</template>
+
+<style scoped>
+
+.listagem-produtos{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: stretch;
+}
+ .listagem-livros p{
+  margin: 0;
+ }
+
+.card-produto{
+  background-color: rgba(137, 43, 226, 0.719);
+  padding: 10px;
+  border-radius: 10px;
+  width: 600px;
+  margin: 10px;
+}
+.produto-imagem{
+  width: 60px;
+}
+
+
+
+
+
+
+</style>
---
+++ src/_data/produtos.js
@@ -0,0 +1,56 @@
+
+
+
+export const produtos = [
+    {
+  
+  
+          id: 1,
+          nome: 'TAYLOR SWIFT THE ERAS TOUR BEIGE HOODIE',
+          price: 75.00,
+          image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/products/3-9vs2eLiKqUbbgJ1pDHAb_600x.png'
+          
+      },
+      {
+          id: 2,
+          nome: 'TAYLOR SWIFT THE ERAS TOUR BLACK SWEATPANTS',
+          price: 65.00,
+          image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/products/IgdoUiadxZX0iC5E82yW_600x.png'
+      },
+      {
+          id: 3,
+          nome: 'TAYLOR SWIFT THE ERAS TOUR CROPPED BEIGE PULLOVER',
+          price: 65.00,
+          image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/products/1-q7e5oRiAkmuDbxTZJfw9_600x.png'
+      },
+      {
+          id: 4,
+          nome: 'TAYLOR SWIFT THE ERAS TOUR MUG',
+          price: 20.00,
+          image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/products/oIZZZm2EA3Y3mzAFIvZw-1_600x.png'
+          
+        
+      },
+      {
+          id: 5,
+          nome: 'TAYLOR SWIFT THE ERAS TOUR GUITAR PICKS',
+          price: 15.00,
+          image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/products/8bwyIsKWVqncNP3c6UNO_600x.png'
+          
+      },
+      {
+          id: 6,
+          nome: 'TAYLOR SWIFT THE ERAS TOUR POSTER',
+          price: 30.00,
+          image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/products/1-q1yFbygO9piJLyvlbf7V_600x.png'
+          
+      },
+      {
+          id: 7,
+          nome: 'TAYLOR SWIFT THE ERAS TOUR PORTABLE CHARGER',
+          price: 30.00,
+          image: 'https://cdn.shopify.com/s/files/1/0011/4651/9637/products/PMlsgxT2JPkKDxc7C7uQ_600x.png'
+         
+      }
+  ]
+  
